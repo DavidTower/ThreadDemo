879fb161a73cb175fe65c080ecff67238ef26da2
lvsonghai <747923860@qq.com>
2018-07-18 14:25:58 +0800

    创建GCD文件

---
+++ ThreadDemo/GCDDemo/GCDDemo.h
@@ -0,0 +1,13 @@
+//
+//  GCDDemo.h
+//  ThreadDemo
+//
+//  Created by CSMBP on 2018/7/18.
+//  Copyright © 2018年 josh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCDDemo : NSObject
+
+@end
---
+++ ThreadDemo/GCDDemo/GCDDemo.m
@@ -0,0 +1,13 @@
+//
+//  GCDDemo.m
+//  ThreadDemo
+//
+//  Created by CSMBP on 2018/7/18.
+//  Copyright © 2018年 josh. All rights reserved.
+//
+
+#import "GCDDemo.h"
+
+@implementation GCDDemo
+
+@end
